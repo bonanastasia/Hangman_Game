8565e2b14344e70cec683cbf0276e52ba88de66a
Anastasia Marie <ana.goodwin10@gmail.com>
2017-03-17 23:41:06 -0500

    version to turn in

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,171 +1,97 @@
 
-// startGame();
 
-// function startGame(){
+//this array holds the word options
+var wordChoices = ["hombres", "taxes", "emails", "bigly", "illegals", "hillary", "liberal", "russia", "loser", "tremendous", "nasty" ];
 
-
-
-
-//something to hold words (array)
-var wordChoices = ["wordon", "wordtwo", "wordthree" ];
-//some way to randomly select a word from the array (Math.floor(Math.random()*array.length))
-// var currentWord = wordChoices[Math.floor(Math.random()*wordChoices.length)];
+// randomly selected word is pushed to this array
 var currentWord=[];
+
+// generates random word from wordChoices array
 var generateWord=function(){
 	currentWord=wordChoices[Math.floor(Math.random()*wordChoices.length)];
 };
 
-			// //a counter for wins (var countWins)
-			// var countWins=0;
-			// //a counter for losses (var countLosses)
-			// var countLosses=0;
-			// //derive from incorrect guesses
-			// var guessesLeft6;
-			// var displayWord=[];
-			// var correctGuesses=[];
-			// var incorrectGuesses=[];
-			// //take correctGuesses and currentWord, compare with loop
-    //use myString.split('')
-//something to hold the "skeleton" (letter holders)
-
-//compare skeletonWord to presence of placeholders
 var wordComplete=false;
-//invoke this on NEW WORD
 
-// var letterChecker = function(){
-//      currentWord.split('').forEach(function(c){
-//      if(correctGuesses.indexOf(c)===-1){
-//          skeletonWord+="_";
-//          isWordComplete=false;
-//      }else{
-//      skeletonWord+= userGuess;
-        //Check to see if word is complete by if there are any placeholders
-            //**if(skeletonWord.charAt
-        // }
- // });
-// };
-//get value
-//evaluate guess( if correct, add to correct guesses if incorrect, add
-// //iterate through current word
-// for(var i=0; i<currentWord.length; i++){
-//  //only add letter to correct guesses if the character is found AND a
-//  //unique character
-// if(userGuess === currentWord.charAt(i) &&  correctGuess.indexOf(i)===-1){
-//  correctGuesses+=i;
-//  letterChecker(userGuess);
-//  console.log("the new word is " + skeletonWord + "correct guesses:" + correctGuesses);
-// }else {
-//  incorrectGuesses+=i;
-//  console.log("the new word is " + skeletonWord + " incorrect Guesses: "+ incorrectGuesses);
-// }
-// }
-//to incorrect guess----derive guesses left)
-//create function that (evaluates guess, current word) then add to correct of incorrect guess arrays
-//create new string newSkeletonWord=[]; forEach if userInput if currentWord.split('')
-//something to capture keystrokes (key.event?)
-//get value
-// for (var i = 0; i < currentWord.length; i++) {
-//      displayWord[i] = "_";
-// };
+// declare counter outside gameStart function so it does not get reset
+var countWins=0;
 
-// var initialVariables = function(){
-// countWins = 0;
-// //a counter for losses (countLosses)
-// countLosses = 0;
-// //derive from incorrect guesses
-// guessesLeft = 6;
-// displayWord = [];
-// correctGuesses = [];
-// incorrectGuesses =[] ;
-// generateWord();
-// currentWord="";
-// wordComplete=false;
-
-// };
-
-// for (var i = 0; i < currentWord.length; i++) {
-//      displayWord[i] = "_";
-// };
-
-
-// var getGuesses = function(c){
-// 		if(correctGuesses.indexOf(c)===-1 && displayWord.indexOf(c)!=-1){
-// 			correctGuesses+=c;
-// 		}else if(correctGuesses.indexOf(c)===-1 && alphabet.includes(c)){
-// 			incorrectGuesses += c;
-// 	};
-// };
-countWins=0;
-
-
-
-// var isWordComplete= function(){
-// 	displayWord.forEach(function(){
-// 		if(displayWord.indexOf("_")===-1){
-// 			if(wordComplete=true){
-// 				startGame();
-// 			}
-
-// 			// countWins++;
-// 			// console.log(countWins);
-// 			// gameReset();
-
-// 		}else{
-// 			wordComplete=false;
-// 		};
-// 	});
-// };
-
-
+// wrap whole game in function to be called on page-load
 var startGame= function(){
- //get element from html & connect	
+
+ //get element from html & connect to javascript	
 var placeholdersWord = document.getElementById("placeholder-word"); 
 var rightGuesses = document.getElementById("right-guesses");
 var wrongGuesses = document.getElementById("wrong-guesses");
 var remainingGuesses = document.getElementById("guesses-remaining-count");
 var resetButton = document.getElementById("reset");
 var winCounter =  document.getElementById("win-count");
-
 resetButton.onclick= reloadPage;
 
-function reloadPage(){
+// reloads page by refreshing page
+var reloadPage= function (){
    window.location.reload();
 };
 
+// insert win count into HTML
 winCounter.innerHTML=countWins;
 
+// determines if you've won the whole game. If you have(10 wins), 
+// the background changes and the game win count is sent back to zero.
 var marryPutin=function(){
 	if(countWins>1){
+		document.body.classList.add("won-game");
 		alert("you and Putin can finally run away together!")
 		countWins=0;
-		var playAgain=confirm("would you like to play again?");
-			if(playAgain){
-				window.location.reload();
-
-			};
 		
 	};
 };
-//a counter for losses (var countLosses)
-//derive from incorrect guesses
+
+var youLose=function(){
+	if(newGuessesLeft===0){
+	document.body.classList.add("lost-game");
+	document.getElementById("impeach").style.display="flex";
+	}
+}
+	
+
+//declare initial variables for start of game play
 var guessesLeft=6;
 var displayWord=[];
 var correctGuesses=[];
 var incorrectGuesses=[];
 var wordComplete=false;
 
+// function makes sure the letter input is in the alphabet, and determines
+// if it is a correct or incorrect guess. also calculates guesses left.
+
 var getGuesses = function(c){
 		if(correctGuesses.indexOf(c)===-1 && displayWord.indexOf(c)!=-1){
+			// add to correctGuesses
 			correctGuesses+=c;
+			// insert array into HTML
 			rightGuesses.innerHTML = correctGuesses;
 
 		}else if(incorrectGuesses.indexOf(c)===-1 && alphabet.includes(c)){
+			// add to incorrectGuesses
 			incorrectGuesses += c;
+
+			
+			// add to HTML
 			wrongGuesses.innerHTML = incorrectGuesses;
+			// determine amount of guesses left
 			remainingGuesses.innerHTML=guessesLeft-incorrectGuesses.length;
+			newGuessesLeft=guessesLeft-incorrectGuesses.length;
+			youLose();	
+			
+
 
 	};
-};
+};		
+	
+// function checks to see if the word still has any placeholders.
+// if it does not (is complete), changes wordComplete to true.
+
 var isWordComplete= function(){
 	displayWord.forEach(function(){
 		if(displayWord.indexOf("_")===-1){
@@ -178,62 +104,57 @@ var isWordComplete= function(){
 	});
 };
 
+// function resets the game, adds to win count, checks to see if you've won 10 times, 
+// and restarts game play to generate new word.
 var resetGame= function(){
 	countWins++;
 	marryPutin();
 	startGame();
 
 }
-
+ // at start of game, we need a new word. Call the generateWord function.
  generateWord();
  console.log(currentWord);
 
+// creates check to make sure input is a letter, and only a letter. split("") creates array for easier iteration.
 var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
-
+// at start of round, iterate through currentWord and create placeholder indexes
 for (var i = 0; i < currentWord.length; i++) {
 displayWord[i] = "_";
+
+// display placeholder version in HTML
 placeholdersWord.innerHTML = displayWord;
 };
 
 
-
+// listen for key event
 document.onkeyup = function(event) {
-        // Determines which key was pressed
-
-        // initialVariables();
-
-      //   for (var i = 0; i < currentWord.length; i++) {
-     	// displayWord[i] = "_";
-		// };
+	// determine which key was pressed
+    var userGuess = event.key;
 
 
-
-
-       
-        var userGuess = event.key;
-
-        // generateWord();
-        // console.log(currentWord);
-    
-        // Alerts the key the user pressed (userGuess).
-        alert(userGuess);
-     //iterate through current word
+     //iterate through "currentWord" and compare to userGuess
     for(var i=0; i<currentWord.length; i++){
-        //only add letter to correct guesses if the character is found AND a
-        //unique character
-    if(userGuess === currentWord.charAt(i)){
-          displayWord[i] = userGuess;
-          placeholdersWord.innerHTML = displayWord;
-          isWordComplete();
-          if(wordComplete===true){
-          	resetGame();
-          	// marryPutin();
-          }
-	   };
+        
+        // if userGuess matches a character in currentWord, replace index with character input from userGuess
+	    if(userGuess === currentWord.charAt(i)){
+	          displayWord[i] = userGuess;
 
-    //  displayWord.push("_ ");
-    // }
+	          // in HTML, show updated placeholder word
+	          placeholdersWord.innerHTML = displayWord;
+
+	          // check if the word is complete
+	          isWordComplete();
+
+	          // if word is complete, reset Game using resetGame()
+	          if(wordComplete===true){
+	          	resetGame();
+	         
+	          }
+		};
+
+  
     };
     getGuesses(userGuess);
  
@@ -241,13 +162,10 @@ document.onkeyup = function(event) {
     console.log("correct: "+ correctGuesses);
     console.log("incorrect: "+ incorrectGuesses);
     console.log(wordComplete);
- //        something to compare user input to letters in currentWord (conditional?
-    // if match, then display (innerHtml, write.document?) in word, if not,
 
- };
-//something to compare user input to letters in currentWord (conditional?
-// if match, then display (innerHtml, write.document?) in word, if not,
-// display in loss)
+
+};
+
 };
 startGame();
 
